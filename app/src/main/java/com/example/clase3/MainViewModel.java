0e2d57634e1be4f384e7eeb09ce6d7ebb0b4f3ff
Elio Hoyos <elio18david@gmail.com>
2024-09-06 10:01:38 -0500

    score

---
+++ app/src/main/java/com/example/clase3/MainViewModel.java
@@ -0,0 +1,12 @@
+package com.example.clase3;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+
+    //declarar dos clases de java
+    private final MutableLiveData<Integer> localScore = new MutableLiveData<>();
+    
+
+}
