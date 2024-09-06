205cc4bce4728625f218c0716a42658d38371576
Elio Hoyos <elio18david@gmail.com>
2024-09-06 11:05:34 -0500

    metodo para sumar y restar al local y visitante

--- app/src/main/java/com/example/clase3/MainActivity.java
+++ app/src/main/java/com/example/clase3/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.clase3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.clase3.databinding.ActivityMainBinding;
 
@@ -10,11 +12,54 @@ public class MainActivity extends AppCompatActivity {
 
     //variables
     private ActivityMainBinding binding;
+    private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+
+
     }
+
+    //método
+    private void setupButtons(){
+
+        binding.localMinusButton.setOnClickListener(v -> {
+
+        });
+
+        binding.localPlusButton.setOnClickListener(v -> {
+
+        });
+
+        binding.localTwoPointsButton.setOnClickListener(v -> {
+
+        });
+
+        binding.visitorMinusButton.setOnClickListener(v -> {
+
+        });
+
+        binding.visitorPlusButton.setOnClickListener(v -> {
+
+        });
+
+        binding.visitorTwoPointsButton.setOnClickListener(v -> {
+
+        });
+
+        binding.restarButton.setOnClickListener(v -> {
+
+        });
+
+        binding.resultButton.setOnClickListener(v -> {
+
+        });
+
+    }
+
 }
--- app/src/main/java/com/example/clase3/MainViewModel.java
+++ app/src/main/java/com/example/clase3/MainViewModel.java
@@ -1,5 +1,6 @@
 package com.example.clase3;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
@@ -7,6 +8,47 @@ public class MainViewModel extends ViewModel {
 
     //declarar dos clases de java
     private final MutableLiveData<Integer> localScore = new MutableLiveData<>();
-    
+    private final MutableLiveData<Integer> visitorScore = new MutableLiveData<>();
+
+    //métodos
+    public MainViewModel(){
+
+    }
+
+    public LiveData<Integer> getLocalScore(){
+        return localScore;
+    }
+
+    public LiveData<Integer> getVisitorScore(){
+        return visitorScore;
+    }
+
+    //método para inicializar juego en 0 local y visitante
+    void resetScores(){
+        localScore.setValue(0);
+        visitorScore.setValue(0);
+    }
+
+    //método para agregar puntos al local o visitante
+    void addPointsToScore(int points, Boolean isLocal){
+        if(isLocal){
+            localScore.setValue(localScore.getValue() + points);
+        }else {
+            visitorScore.setValue(visitorScore.getValue() + points);
+        }
+    }
+    //método para restar -1 al local
+    void decreaseLocal(){
+        if(localScore.getValue() > 0){
+            localScore.setValue(localScore.getValue() -1);
+        }
+    }
+
+    //método para restar -1 al visitante
+    void decreaseVisitor(){
+        if(visitorScore.getValue() > 0){
+            visitorScore.setValue(visitorScore.getValue() -1);
+        }
+    }
 
 }
